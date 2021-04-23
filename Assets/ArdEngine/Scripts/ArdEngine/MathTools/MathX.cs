d60ef574756c1fedf54e2987a630f247ca19b55e
Radek <radek@blindflugstudios.com>
2021-04-23 23:10:29 +0200

    Renamed math class

--- Assets/ArdEngine/Scripts/ArdEngine/MathTools/MathX.cs
+++ Assets/ArdEngine/Scripts/ArdEngine/MathTools/MathX.cs
@@ -3,7 +3,7 @@ using JetBrains.Annotations;
 
 namespace ArdEngine.MathTools
 {
-    public static class MathEx
+    public static class MathX
     {
         [Pure] [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Mod(this int value, int mod)
